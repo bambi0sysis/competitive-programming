f98fac09965040996a1423c5f5347ed07c6adb02
bambi0sysis <andidotooandmaybeyoutoo@gmail.com>
2026-07-01 20:07:31 +0530

    Day 2 (July 1): CPH configured + fixed IntelliSense include paths + verified local execution

--- basics/demo.cpp
+++ basics/demo.cpp
@@ -1,9 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void DoSomething(int arr[], int n)
+{
+    for (int i = 0; i < n; i += 1)
+        cin >> arr[i];
+}
+
+void PrintOutput(int arr[], int n)
+{
+    for (int i = 0; i < n; i += 1)
+    {
+        cout << arr[i] << endl;
+    }
+}
+
 int main()
 {
-    int x;
-    cin >> x;
-    cout << "Hey " << x;
+    int n;
+    cin >> n;
+    int arr[n];
+    DoSomething(arr, n);
+    PrintOutput(arr, n);
+    return 0;
 }
