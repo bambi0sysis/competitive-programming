dbd4e686d8b720646a6b4eb2009f2271eec9f454
bambi0sysis <andidotooandmaybeyoutoo@gmail.com>
2026-07-06 20:21:27 +0530

    A2Z Basic Math: Reverse Integer with overflow guard

--- basics/practice.cpp
+++ basics/practice.cpp
@@ -16,12 +16,27 @@ bool isPalindrome(int x)
     return false;
 }
 
+int reverse(int x)
+{
+    int num = 0;
+    while (x != 0)
+    {
+        int ld = x % 10;
+        x /= 10;
+
+        // Check for overflow BEFORE updating num
+        if (num > INT_MAX / 10 || num < INT_MIN / 10)
+        {
+            return 0;
+        }
+
+        num = (num * 10) + ld;
+    }
+    return num;
+}
+
 int main()
 {
-    int t;
-    cin >> t;
-    if (isPalindrome(t) == 1)
-        cout << "true";
-    else
-        cout << "false";
+    int x = 1534236469;
+    cout << reverse(x);
 }
